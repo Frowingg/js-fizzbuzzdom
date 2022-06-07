026895afcefdaf2c1aff891ef70c000c1b4dfbd0
Frowingg <edbico0@gmail.com>
2022-06-07 15:27:19 +0200

    MILESTONE 2

---
+++ MILESTONE_2/script.js
@@ -0,0 +1,16 @@
+const mainList = document.querySelector('.list');
+
+
+for(let i=0; i <= 1000; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        newLi = `<li>FizzBuzz</li>`;
+    } else if (i % 3 === 0) {
+        newLi = `<li>Fizz</li>`;
+    } else if (i % 5 === 0) {
+        newLi = `<li>Buzz</li>`;
+    } else {
+        newLi = `<li>${i}</li>`
+    }
+    mainList.innerHTML += newLi;
+} 
+
